6a3282c7de35e927def561962d36b9fb1a3e9a11
Mo Zeumer <m_zeumer@gmx.de>
2018-11-04 03:16:54 +0100

    Skybox PosList update 04-11

--- KesselRunGame/KesselRunGame/classes/Skybox.cs
+++ KesselRunGame/KesselRunGame/classes/Skybox.cs
@@ -4,13 +4,39 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using OpenTK;
+using OpenTK.Graphics.OpenGL;
+
+using cgimin.engine.object3d;
+
 namespace KesselRunGame
 {
-    class Skybox
+    class Skybox : BaseObject3D
     {
+
         public Skybox()
         {
+            Positions = new List<Vector3>
+            {
+                new Vector3(0, 0, 0),
+                new Vector3(0, 110, 0),
+                new Vector3(110, 110, 0),
+                new Vector3(110, 0, 0),
+                new Vector3(0, 0, 110),
+                new Vector3(0, 110, 110),
+                new Vector3(110, 110, 110),
+                new Vector3(110, 0, 110)
+            };
+            UVs = new List<Vector2>();
+            Normals = new List<Vector3>();
+            Indices = new List<int>();
 
+
+            addTriangle(new Vector3(0, 1, 0), new Vector3(1, 1, 0), new Vector3(1, 2, 0), new Vector3(1, 1, 1), new Vector3(1, 1, 1), new Vector3(1, 1, 1),
+                        new Vector2(0, 0), new Vector2(1, 0), new Vector2(1, 1));
+
+
+            CreateVAO();
         }
     }
 }

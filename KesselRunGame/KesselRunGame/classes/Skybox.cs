89a16498b5a84c8402a31381f482eb1bf5658a41
Mo Zeumer <m_zeumer@gmx.de>
2018-11-17 00:29:32 +0100

    Skybox UV diagram added

--- KesselRunGame/KesselRunGame/classes/Skybox.cs
+++ KesselRunGame/KesselRunGame/classes/Skybox.cs
@@ -20,13 +20,15 @@ namespace KesselRunGame.classes
         /// <summary> Skybox Material </summary>
         private SimpleTextureMaterial material;
 
-        public Skybox()
+        /// <summary>
+        /// Constructor to generate a new Skybox
+        /// </summary>
+        /// <param name="size">Dimensions of the Skybox</param>
+        /// <param name="SkyboxTexturePath">Path to the Skybox Texture</param>
+        public Skybox(float size = 100.0f, string SkyboxTexturePath = "data/textures/cubemapTemplate.png")
         {
-            Skybox(100.0f, "data/textures/cubemapTemplate")
-        }
-
-        public Skybox(float size, string SkyboxTexturePath)
-        {
+            // List of Corner-Points
+            //
             //      Y
             //      |
             //      2-------3
@@ -37,7 +39,7 @@ namespace KesselRunGame.classes
             //    5-------8
             //   /
             //  Z
-            // List of Corner-Points
+            //
             List<Vector3> corners = new List<Vector3>
             {
                 new Vector3(0.0f),  // 0
@@ -50,37 +52,45 @@ namespace KesselRunGame.classes
                 new Vector3(size, size, size),  // 7
                 new Vector3(size, 0.0f, size)   // 8
             };
-            // Inverted Unit-Vectors
-            Vector3 invX = new Vector3(-1.0f, 0.0f, 0.0f);
-            Vector3 invY = new Vector3(0.0f, -1.0f, 0.0f);
-            Vector3 invZ = new Vector3(0.0f, 0.0f, -1.0f);
 
             // setting Skybox-Faces  TODO: UV vectors
-                // Left Face (-X)
-            skyboxObject.addTriangle(corners[1], corners[2], corners[6], Vector3.UnitX, Vector3.UnitX, Vector3.UnitX, new Vector2(0.0f, 0.0f), Vector2.Zero, Vector2.Zero);  // 1-2-6
-            skyboxObject.addTriangle(corners[1], corners[5], corners[6], Vector3.UnitX, Vector3.UnitX, Vector3.UnitX, Vector2.Zero, Vector2.Zero, Vector2.Zero);  // 1-5-6
-                // Right Face (+X)
-            skyboxObject.addTriangle(corners[8], corners[7], corners[3], invX, invX, invX, Vector2.Zero, Vector2.Zero, Vector2.Zero);  // 8-7-3
-            skyboxObject.addTriangle(corners[3], corners[4], corners[8], invX, invX, invX, Vector2.Zero, Vector2.Zero, Vector2.Zero);  // 3-4-8
-                // Bottom Face (-Y)
-            skyboxObject.addTriangle(corners[1], corners[5], corners[8], Vector3.UnitY, Vector3.UnitY, Vector3.UnitY, Vector2.Zero, Vector2.Zero, Vector2.Zero);  // 1-5-8
-            skyboxObject.addTriangle(corners[1], corners[4], corners[8], Vector3.UnitY, Vector3.UnitY, Vector3.UnitY, Vector2.Zero, Vector2.Zero, Vector2.Zero);  // 1-4-8
-                // Top Face (+Y)
-            skyboxObject.addTriangle(corners[2], corners[3], corners[7], invY, invY, invY, Vector2.Zero, Vector2.Zero, Vector2.Zero);  // 2-3-7
-            skyboxObject.addTriangle(corners[2], corners[6], corners[7], invY, invY, invY, Vector2.Zero, Vector2.Zero, Vector2.Zero);  // 2-6-7
-                // Back Face (-Z)
-            skyboxObject.addTriangle(corners[1], corners[2], corners[3], Vector3.UnitZ, Vector3.UnitZ, Vector3.UnitZ, Vector2.Zero, Vector2.Zero, Vector2.Zero);  // 1-2-3
-            skyboxObject.addTriangle(corners[3], corners[4], corners[1], Vector3.UnitZ, Vector3.UnitZ, Vector3.UnitZ, Vector2.Zero, Vector2.Zero, Vector2.Zero);  // 3-4-1
-                // Front Face (+Z)
-            skyboxObject.addTriangle(corners[5], corners[6], corners[7], invZ, invZ, invZ, Vector2.Zero, Vector2.Zero, Vector2.Zero);  // 5-6-7
-            skyboxObject.addTriangle(corners[5], corners[8], corners[7], invZ, invZ, invZ, Vector2.Zero, Vector2.Zero, Vector2.Zero);  // 5-8-7
-
+            //
+            //       2----3           - 3/3
+            //       | +Y |                    2------3    3
+            //  2----6----7----3----2 - 2/3    |    _/   _/|
+            //  | -X | +Z | +X | -Z |          |  _/   _/  |
+            //  1----5----8----4----1 - 1/3    |_/   _/    |
+            //       | -Y |                    1    1------2
+            //       1----4           - 0/3
+            //  |    |    |    |    |
+            // 0/4  1/4  1/2  3/4  4/4
+            //
+            // Left Face (-X)
+            skyboxObject.addTriangle(corners[1], corners[2], corners[6], Vector3.UnitX, Vector3.UnitX, Vector3.UnitX, new Vector2(), new Vector2(), new Vector2());  // 1-2-6
+            skyboxObject.addTriangle(corners[1], corners[5], corners[6], Vector3.UnitX, Vector3.UnitX, Vector3.UnitX, new Vector2(), new Vector2(), new Vector2());  // 1-5-6
+            // Right Face (+X)
+            skyboxObject.addTriangle(corners[8], corners[7], corners[3], -Vector3.UnitX, -Vector3.UnitX, -Vector3.UnitX, new Vector2(), new Vector2(), new Vector2());  // 8-7-3
+            skyboxObject.addTriangle(corners[8], corners[4], corners[3], -Vector3.UnitX, -Vector3.UnitX, -Vector3.UnitX, new Vector2(), new Vector2(), new Vector2());  // 3-4-8
+            // Bottom Face (-Y)
+            skyboxObject.addTriangle(corners[1], corners[5], corners[8], Vector3.UnitY, Vector3.UnitY, Vector3.UnitY, new Vector2(), new Vector2(), new Vector2());  // 1-5-8
+            skyboxObject.addTriangle(corners[1], corners[4], corners[8], Vector3.UnitY, Vector3.UnitY, Vector3.UnitY, new Vector2(), new Vector2(), new Vector2());  // 1-4-8
+            // Top Face (+Y)
+            skyboxObject.addTriangle(corners[6], corners[2], corners[3], -Vector3.UnitY, -Vector3.UnitY, -Vector3.UnitY, new Vector2(), new Vector2(), new Vector2());  // 2-3-7
+            skyboxObject.addTriangle(corners[6], corners[7], corners[3], -Vector3.UnitY, -Vector3.UnitY, -Vector3.UnitY, new Vector2(), new Vector2(), new Vector2());  // 2-6-7
+            // Back Face (-Z)
+            skyboxObject.addTriangle(corners[4], corners[3], corners[2], Vector3.UnitZ, Vector3.UnitZ, Vector3.UnitZ, new Vector2(), new Vector2(), new Vector2());  // 1-2-3
+            skyboxObject.addTriangle(corners[4], corners[1], corners[2], Vector3.UnitZ, Vector3.UnitZ, Vector3.UnitZ, new Vector2(), new Vector2(), new Vector2());  // 3-4-1
+            // Front Face (+Z)
+            skyboxObject.addTriangle(corners[5], corners[6], corners[7], -Vector3.UnitZ, -Vector3.UnitZ, -Vector3.UnitZ, new Vector2(), new Vector2(), new Vector2());  // 5-6-7
+            skyboxObject.addTriangle(corners[5], corners[8], corners[7], -Vector3.UnitZ, -Vector3.UnitZ, -Vector3.UnitZ, new Vector2(), new Vector2(), new Vector2());  // 5-8-7
+            // Move Origin to Cube Center
             skyboxObject.Transformation *= Matrix4.CreateTranslation(-(size / 2), -(size / 2), -(size / 2));
             
-
+            // ----------------------
             // setting Skybox-Texture
             texture = TextureManager.LoadTexture(SkyboxTexturePath);
             
+            // -----------------------
             // setting Skybox-Material
             material = new SimpleTextureMaterial();
         }
